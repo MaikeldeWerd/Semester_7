1c39ec66e53cf49ce7dec36b133288eed53371e5
Maikel De Werd <maikeldewerd@hotmail.com>
2024-03-04 16:26:07 +0100

    Test

---
+++ src/pages/LO.js
@@ -0,0 +1,16 @@
+import Layout from "../components/layouts/Layout";
+import Container from "../components/layouts/Container";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Container>
+        <div>
+          <p>test</p>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
+
+export default Home;
