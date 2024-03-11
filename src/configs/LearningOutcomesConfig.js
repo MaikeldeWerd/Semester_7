7c4b68c953103ee2d9883a85a8472ea5486ba89c
Maikel De Werd <maikeldewerd@hotmail.com>
2024-03-11 13:06:58 +0100

    Learning outcome config

---
+++ src/configs/LearningOutcomesConfig.js
@@ -0,0 +1,29 @@
+import image from '/public/next.svg'
+
+const LearningOutcomesConfig = {
+    outcomes: [
+        {
+            title: 'test',
+            sub_title: 'Learning outcome 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut consequatur consequuntur dignissimos, excepturi facilis hic ipsa, iure neque nihil nisi odio provident quam quis rem reprehenderit similique tempore totam.',
+            image: {image},
+            route: '',
+        },
+        {
+            title: 'test',
+            sub_title: 'Learning outcome 2',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut consequatur consequuntur dignissimos, excepturi facilis hic ipsa, iure neque nihil nisi odio provident quam quis rem reprehenderit similique tempore totam.',
+            image: {image},
+            route: '',
+        },
+        {
+            title: 'test',
+            sub_title: 'Learning outcome 3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut consequatur consequuntur dignissimos, excepturi facilis hic ipsa, iure neque nihil nisi odio provident quam quis rem reprehenderit similique tempore totam.',
+            image: {image},
+            route: '',
+        },
+    ],
+};
+
+export default LearningOutcomesConfig;
